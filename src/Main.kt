365c8d63b2e58046238dd35df03439df6111c063
brianto <alexandre.brianto29@gmail.com>
2026-05-05 09:17:24 +0200

    working first page with html, added readfile function

--- src/Main.kt
+++ src/Main.kt
@@ -6,19 +6,26 @@ import com.sun.net.httpserver.HttpServer // Serveur HTTP
 import com.sun.net.httpserver.HttpHandler // Gestionnaire de requêtes HTTP
 import com.sun.net.httpserver.HttpExchange // Représente une requête/réponse HTTP
 import java.net.InetSocketAddress // Adresse IP et port du serveur
+import java.io.File
+import java.io.InputStream
+
 
 
 fun main() {
     val server = HttpServer.create(InetSocketAddress(8080), 0)
-    server.createContext("/", MyHandler())
-    server.createContext("/a", MyHandler())
+    server.createContext("/", MyHandler("src/templates/pages/accueil.html"))
     //server.executor = null // creates a default executor
     server.start()
 }
 
-class MyHandler : HttpHandler {
+fun readFile(page: String): String {
+    val inputString = File(page).readText()
+    return inputString
+}
+
+class MyHandler(val page: String) : HttpHandler {
     override fun handle(t: HttpExchange) {
-        val response = "<h1>Hello World</h1>"
+        val response = readFile(page)
         t.sendResponseHeaders(200, response.length.toLong())
         val os = t.responseBody
         os.write(response.toByteArray())
